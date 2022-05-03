ea087b40a719e8094ad9f4c9694a71fe33311bc0
VictorTamayoBriones <66786892+VictorTamayoBriones@users.noreply.github.com>
2022-05-02 23:18:07 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+
+  return (
+    <>
+      <h2>Carrito de compas</h2>
+      <p>Producto</p>
+      <article className="box">
+        <p>Carrito</p>
+      </article>
+    </>
+  )
+}
+
+export default App
---
+++ src/reducers/cartReducers.js
@@ -0,0 +1,57 @@
+import { TYPES } from '../actions/cartActions';
+
+export const CART_INITIAL_STATE = {
+    products:[
+        {
+            id: 1,
+            name: "producto 1",
+            price: 100
+        },
+        {
+            id: 2,
+            name: "producto 1",
+            price: 200
+        },
+        {
+            id: 3,
+            name: "producto 1",
+            price: 300
+        },
+        {
+            id: 4,
+            name: "producto 1",
+            price: 400
+        },
+        {
+            id: 5,
+            name: "producto 1",
+            price: 500
+        },
+        {
+            id: 6,
+            name: "producto 1",
+            price: 600
+        },
+    ],
+    cart: []
+}
+
+export function cartReducer(state, action){
+    switch (action.type) {
+        case TYPES.ADD_TO_CART:{
+            
+        }
+        case TYPES.REMOVE_ONE_FROM_CART:{
+
+        }
+        case TYPES.REMOVE_ALL_FROM_CART:{
+
+        }
+        case TYPES.CLEAR:{
+
+        }
+    
+        default:
+            return state;
+    }
+}
